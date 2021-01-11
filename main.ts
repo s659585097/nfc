001fd7b6e3ef56c63f39ab20581318cde170edbb
s659585097 <67514298+s659585097@users.noreply.github.com>
2021-01-11 13:08:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 NFC.nfcEvent(function () {
     makerbit.showStringOnLcd1602(NFC.getUID(), makerbit.position1602(LcdPosition1602.Pos3), 16)
+    if (NFC.getUID() == "85BCDF30") {
+        basic.showIcon(IconNames.Yes)
+    } else if (NFC.getUID() == "3523BC30") {
+        basic.showIcon(IconNames.Yes)
+    } else if (NFC.getUID() == "C5B3BD30") {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
 NFC.NFC_setSerial(SerialPin.P13, SerialPin.P14)
 basic.forever(function () {
