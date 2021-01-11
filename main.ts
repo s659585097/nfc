264c156760571f64025da5a48d77353e86962da4
s659585097 <67514298+s659585097@users.noreply.github.com>
2021-01-11 16:36:36 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,22 @@ NFC.nfcEvent(function () {
     makerbit.showStringOnLcd1602(NFC.getUID(), makerbit.position1602(LcdPosition1602.Pos3), 16)
     if (NFC.getUID() == "85BCDF30") {
         basic.showIcon(IconNames.Yes)
+        music.playMelody("C C G G A A G - ", 120)
     } else if (NFC.getUID() == "3523BC30") {
         basic.showIcon(IconNames.Yes)
+        music.playMelody("C C G G A A G - ", 120)
     } else if (NFC.getUID() == "C5B3BD30") {
         basic.showIcon(IconNames.Yes)
+        music.playMelody("C C G G A A G - ", 120)
     } else {
         basic.showIcon(IconNames.No)
+        music.playMelody("D - D - D - D - ", 120)
     }
+    basic.pause(100)
+    basic.clearScreen()
 })
+dfplayermini.connect(SerialPin.P0, SerialPin.P1)
+dfplayermini.playLoopAllFiles()
 NFC.NFC_setSerial(SerialPin.P13, SerialPin.P14)
 basic.forever(function () {
 	
