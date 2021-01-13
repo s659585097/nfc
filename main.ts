cb5647f117acb01c8ce707e6e64721ac722e9805
s659585097 <67514298+s659585097@users.noreply.github.com>
2021-01-13 16:18:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
 NFC.nfcEvent(function () {
     makerbit.showStringOnLcd1602(NFC.getUID(), makerbit.position1602(LcdPosition1602.Pos3), 16)
     if (NFC.getUID() == "85BCDF30") {
+        robotbit.StepperDegree(robotbit.Steppers.M2, 60)
         basic.showIcon(IconNames.Yes)
         music.playMelody("C C G G A A G - ", 120)
+        robotbit.StepperDegree(robotbit.Steppers.M2, -60)
     } else if (NFC.getUID() == "3523BC30") {
+        robotbit.StepperDegree(robotbit.Steppers.M2, 60)
         basic.showIcon(IconNames.Yes)
         music.playMelody("C C G G A A G - ", 120)
+        robotbit.StepperDegree(robotbit.Steppers.M2, -60)
     } else if (NFC.getUID() == "C5B3BD30") {
+        robotbit.StepperDegree(robotbit.Steppers.M2, 60)
         basic.showIcon(IconNames.Yes)
         music.playMelody("C C G G A A G - ", 120)
+        robotbit.StepperDegree(robotbit.Steppers.M2, -60)
     } else {
         basic.showIcon(IconNames.No)
         music.playMelody("D - D - D - D - ", 120)
